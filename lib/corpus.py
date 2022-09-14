850b5dee63ef78841aa5d225fd5491a1683d87aa
Sami Itkonen <si@iki.fi>
2022-09-14 10:27:14 +0300

    read .vrt file (semi-hardcoded handling)

--- lib/corpus.py
+++ lib/corpus.py
@@ -1,9 +1,9 @@
 """File reader module."""
 
-# pylint: disable=invalid-name, line-too-long, consider-using-f-string
+# pylint: disable=invalid-name, line-too-long, unspecified-encoding
 
 # from typing import List, Dict, Union, Tuple, Optional, Iterable, Callable
-from typing import Tuple, Optional, Iterable, Callable
+from typing import List, Tuple, Optional, Iterable, Callable
 from os import walk
 from os.path import isfile, join, basename
 import re
@@ -29,8 +29,8 @@ def conllu_file_reader(cfile: str,
     if checker:
         # Inspect the checker closure for properties
         try:
-            vars = checker.__code__.co_freevars
-            idx = vars.index('fastcheck')
+            varrs = checker.__code__.co_freevars
+            idx = varrs.index('fastcheck')
             if idx != -1:
                 fastcheck = checker.__closure__[idx].cell_contents  # type: ignore
         except Exception:
@@ -42,26 +42,110 @@ def conllu_file_reader(cfile: str,
                 # print("Fast check, don't read conllu file contents for %s" % cfile)
                 yield 0, None
 
-    with open(cfile, 'r') as f:
-        data = f.read()
+    if cfile.endswith('.vrt'):
+        outorder = {
+            0: 'ref',
+            1: 'lemma',
+            2: 'word',
+            3: 'pos',
+            5: 'msd',
+            6: 'dephead',
+            7: 'deprel',
+            9: 'lex',
+        }
+        inorder = None
 
-    # Get the last sentence id for the file
-    sentids = re.findall(r'# sent_id = (\d+)', data, flags=re.MULTILINE)
-    # print(sentids)
-    sentencecount = max(set([int(s) for s in sentids]))
-    # print(last)
+        with open(cfile, 'r') as f:
+            idx = 0
+            fileidx = 0
+            insentence = False
+            tabidx = [1, 0, 2, 4, 8, 5, 6, 7, 8, 10]
+            sentencedata: List[str] = []
 
-    if checker:
-        has_file = checker(shortfn, sentencecount)
-        # print(cfile, shortfn, sentencecount, has_file)
-        if has_file:
-            yield 0, None
+            for line in tqdm(f):
+                idx += 1
+                if 'positional-attributes' in line:
+                    attrs = re.search(r':\s+(.*)\/', line)
+                    if attrs:
+                        _attrs = attrs.group(1).split()
+                        inorder = {k: i for i, k in enumerate(_attrs)}
+                        # print(inorder)
+                if line.startswith('</sentence'):
+                    insentence = False
+                    fileidx += 1
+                    in_string = ''.join(sentencedata)
+                    # print(in_string)
+                    sentencedata.append('\n')
+                    # c = pyconll.unit.conll.Conll(sentencedata)
+                    c = pyconll.unit.sentence.Sentence(in_string)
+                    sentencedata = []
+                    yield fileidx, c
+                if insentence:
+                    # reformat fields to standard (?) conll-u format
+                    tabs = line.split('\t')
+                    if inorder:
+                        nutabs = []
+                        for _i in range(0, 10):
+                            if _i in outorder:
+                                field = outorder.get(_i)
+                                inidx = inorder[field]
+                                value = tabs[inidx]
+                                # print(field, inidx, value)
+                                if field == 'msd' and value != '_':
+                                    if '_' in value and '=' not in value:
+                                        nuvals = []
+                                        for _v in value.split('|'):
+                                            try:
+                                                if '_' in _v:
+                                                    vals = _v.split('_')
+                                                    k = vals[0]
+                                                    if len(vals) > 2:
+                                                        v = '_'.join(vals[1:])
+                                                    else:
+                                                        v = vals[1]
+                                                    nuvals.append('='.join([k, v]))
+                                                else:
+                                                    nuvals.append(_v)
+                                            except ValueError as e:
+                                                print(_v)
+                                                raise e
+                                        value = '|'.join(nuvals)
+                                nutabs.append(value)
+                            else:
+                                nutabs.append('_')
+                    else:
+                        nutabs = [tabs[i] for i in tabidx]
+                    # print(line)
+                    # print(nutabs)
+                    sentencedata.append('\t'.join(nutabs))
+                if line.startswith('<sentence'):
+                    insentence = True
 
-    # FIXME: field order (?)
-    idx = 0
-    for sentence in pyconll.load_from_file(cfile):
-        idx += 1
-        yield idx, sentence
+                if idx > 20:
+                    pass
+
+        yield 0, ''
+    else:
+        with open(cfile, 'r') as f:
+            data = f.read()
+
+        # Get the last sentence id for the file
+        sentids = re.findall(r'# sent_id = (\d+)', data, flags=re.MULTILINE)
+        # print(sentids)
+        sentencecount = max(set([int(s) for s in sentids]))
+        # print(last)
+
+        if checker:
+            has_file = checker(shortfn, sentencecount)
+            # print(cfile, shortfn, sentencecount, has_file)
+            if has_file:
+                yield 0, None
+
+        # FIXME: field order (?)
+        idx = 0
+        for sentence in pyconll.load_from_file(cfile):
+            idx += 1
+            yield idx, sentence
 
 
 def conllu_freq_reader(path: str,
