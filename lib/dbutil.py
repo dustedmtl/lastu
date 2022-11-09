c2ec006709037b714c534ea7bbb21d7f77056749
Sami Itkonen <si@iki.fi>
2022-11-09 14:42:55 +0200

    query timing, database class, posx/derivation/clitic handling

--- lib/dbutil.py
+++ lib/dbutil.py
@@ -3,13 +3,15 @@
 # pylint: disable=invalid-name, line-too-long
 
 # from typing import List, Dict, Tuple, Optional, Callable, Iterable
-from typing import List, Tuple, Union, Dict
+from typing import List, Tuple, Union, Dict, Optional
 from collections import Counter, defaultdict
+import time
 # from os.path import basename
 # from io import StringIO
 import sqlite3
 from sqlite3 import IntegrityError
 import pandas as pd
+from pandas.io.sql import DatabaseError
 import numpy as np
 import logging
 from tqdm.autonotebook import tqdm
@@ -20,13 +22,48 @@ logger = logging.getLogger('ui-qt6')
 logger.setLevel(logging.DEBUG)
 
 
-# FIXME: make this a class
-# FIXME: store wordfreqs table column list
-# FIXME: record choices from feature tables
 def get_connection(dbfile: str) -> sqlite3.Connection:
     """Get SQLite connection."""
     sqlcon = sqlite3.connect(dbfile)
     return sqlcon
+
+
+def query_timing(func):
+    """Get timing information from a function."""
+
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        results = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f'Query execution took {end - start:.1f} seconds')
+        return results
+    return wrapper
+
+
+@query_timing
+def adhoc_query(connection: sqlite3.Connection,
+                sqlstr: str,
+                verbose: bool = False,
+                todf: bool = False) -> Optional[pd.DataFrame]:
+    """Execute an adhoc query."""
+    try:
+        if verbose:
+            logger.debug('Executing query: %s', sqlstr)
+        if todf:
+            df = pd.DataFrame(pd.read_sql_query(sqlstr, connection))
+            return df
+
+        # print(connection)
+        cursor = connection.cursor()
+        cursor.execute(sqlstr)
+        connection.commit()
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        logging.exception(e)
+        if not todf:
+            connection.rollback()
+        return None
 
 
 def write_freqs_to_db(connection: sqlite3.Connection,
@@ -192,10 +229,63 @@ def insert_bigram_freqs(connection: sqlite3.Connection,
         connection.rollback()
 
 
-def parse_query(query: str) -> List[List[str]]:
+# FIXME: record choices from feature tables
+class DatabaseConnection:
+    """Encapsulation of database connection."""
+
+    def __init__(self, filename: str):
+        """Initialize database connection class."""
+        # super().__init__(args)
+        self.limit = 10000
+        self.dbfile = filename
+        self.connection = get_connection(filename)
+        self.tables = ['wordfreqs']
+        self.columns = defaultdict(list)  # type: ignore
+        self.record_columns()
+
+    def record_columns(self):
+        """Store column lists."""
+        for table in self.tables:
+            # print(table)
+            columndata = adhoc_query(self.connection,
+                                     f'PRAGMA table_info({table})',
+                                     todf=True)
+            cols = columndata.name
+            self.columns[table] = list(cols)
+
+    def rowlimit(self, limit: int = None) -> int:
+        """Get/set rowlimit."""
+        if limit:
+            self.limit = limit
+        return self.limit
+
+    def get_connection(self) -> sqlite3.Connection:
+        """Return SQL connection."""
+        return self.connection
+
+    def get_queryselects(self, useposx: bool) -> str:
+        """Get applicable query selects."""
+        cols = []
+
+        for col in self.columns['wordfreqs']:
+            usecol = 'w.' + col
+            if usecol == 'w.pos':
+                usecol = 'w.posx as pos' if useposx else 'w.pos'
+            elif usecol == 'w.posx':
+                continue
+            elif usecol == 'w.frequency':
+                if useposx:
+                    usecol = "sum(w.frequency) as frequency"
+            cols.append(usecol)
+        return ', '.join(cols)
+
+
+# FIXME: make this a query class
+def parse_query(query: str) -> Tuple[List[List[str]], List]:
     """Parse query to SQL key-values."""
     parts = query.split('and')
     kvparts = []
+    errors: List[str] = []
 
     numkeys = ['frequency', 'len',
                'lemmafreq', 'lemmalen', 'amblemma',
@@ -229,12 +319,12 @@ def parse_query(query: str) -> List[List[str]]:
                             comparator = 'not like'
                             isok = True
                         else:
-                            print(f"Invalid query part: {part}")
+                            errors.append(f"Invalid query part: '{part.strip()}'")
                     else:
                         comparator = 'like'
                         isok = True
                 else:
-                    print(f"Invalid query part: {part}")
+                    errors.append(f"Invalid query part: '{part.strip()}'")
 
             else:
                 key, comparator, value = vals
@@ -248,9 +338,9 @@ def parse_query(query: str) -> List[List[str]]:
                         if value.isnumeric():
                             isok = True
                         else:
-                            print(f"Query value for key '{key}' not ok: '{value}' is not a number")
+                            errors.append(f"Query value for key '{key}' not ok: '{value}' is not a number")
                     else:
-                        print(f"Query comparator for '{key}' not ok: '{comparator}'")
+                        errors.append(f"Query comparator for '{key}' not ok: '{comparator}'")
                 elif key in strkeys:
                     if comparator in stroperators:
                         isok = True
@@ -261,12 +351,12 @@ def parse_query(query: str) -> List[List[str]]:
 
             if isok:
                 kvparts.append([key, comparator, value])
-            # FIXME: compound
             # FIXME: middle parts
-        except Exception as e:
-            print(f"Invalid query part {part}: {e}")
+        except ValueError as e:
+            logging.exception(e)
+            errors.append(f"Invalid query part: '{part.strip()}'")
 
-    return kvparts
+    return kvparts, errors
 
 
 indexorder = ['w.form', 'w.lemma',
@@ -285,15 +375,15 @@ indexfields = {'w.frequency': 'wfreq', 'w.form': 'wform',
                'w.clitic': 'wclitic'}
 
 
-def parse_querystring(querystr: str) -> Tuple[str, List, List, List]:
+def parse_querystring(querystr: str) -> Tuple[str, List, List, List, List, bool]:
     """Parse the query string."""
-    queryparts = parse_query(querystr)
+    queryparts, errors = parse_query(querystr)
     # print(queryparts)
 
     whereparts = []
     wherestr = ""
     args = []
-
+    useposx = True
     gfields = defaultdict(set)  # type: ignore
 
     indexers = []
@@ -310,7 +400,7 @@ def parse_querystring(querystr: str) -> Tuple[str, List, List, List]:
         fullcol = f'{usetable}.{k}'
         if fullcol in indexorder:
             if c == 'like':
-                # % at the beginning of string of like query: no indexing
+                # % not at the beginning of string of like query: no indexing
                 if not v.startswith('%'):
                     indexers.append(fullcol)
             else:
@@ -320,22 +410,41 @@ def parse_querystring(querystr: str) -> Tuple[str, List, List, List]:
         gflist = gfields.get(k, set())
         if c == 'in':
             invals = [w.strip() for w in v.split(',')]
-            if k in ['derivation', 'clitic']:
+            if k in ['cliticx']:
+                # old method, deprecated
+                usetable = k[0]
                 subargs = []
                 whereor = []
                 for val in invals:
                     subargs.append(f'{val}%')
-                    subargs.append(f'%,{val}%')
+                    subargs.append(val)
                     whereor.append(f'{usetable}.{k} LIKE ?')
                     whereor.append(f'{usetable}.{k} LIKE ?')
                 whereparts.append(f"({' OR '.join(whereor)})")
                 args.extend(subargs)
+            elif k in ['derivation', 'clitic']:
+                usetable = k[0]
+                # separate table for derivations and clitics
+                qmarks = ','.join(['?'] * len(invals))
+                whereparts.append(f'{usetable}.{k} {c} ({qmarks})')
+                whereparts.append(f'w.lemma = {usetable}.lemma and w.form = {usetable}.form and w.posx = {usetable}.pos and w.feats = {usetable}.feats')
+                args.extend(invals)
             else:
+                if k == 'pos':
+                    if 'AUX' in invals:
+                        useposx = False
+                    else:
+                        k = 'posx'
                 qmarks = ','.join(['?'] * len(invals))
                 whereparts.append(f'{usetable}.{k} {c} ({qmarks})')
                 args.extend(invals)
             _ = [gflist.add(_) for _ in invals]
         else:
+            if k == 'pos':
+                if v == 'AUX':
+                    useposx = False
+                else:
+                    k = 'posx'
             whereparts.append(f'{usetable}.{k} {c} ?')
             args.append(v)
             if c == '=':
@@ -343,25 +452,27 @@ def parse_querystring(querystr: str) -> Tuple[str, List, List, List]:
         gfields[k] = gflist
 
     if len(whereparts) > 0:
-        wherestr = "WHERE " + ' AND '.join(whereparts)
+        wherestr = "WHERE " + " AND ".join(whereparts)
 
-    return wherestr, args, indexers, notlikeindexers
+    return wherestr, args, errors, indexers, notlikeindexers, useposx
 
 
-def parse_querydict(querydict: Dict) -> Tuple[str, List, List, List]:
+def parse_querydict(querydict: Dict) -> Tuple[str, List, List, List, List]:
     """Parse query dictionary."""
     wherestr = ""
     whereparts = []
     args = []
     indexers = set()
+    errors: List[str] = []
 
+    # FIXME: validate: can only have lemma/form (?)
     for querypart in querydict.keys():
         for queryval in querydict[querypart]:
             indexers.add(querypart)
             whereparts.append(f'w.{querypart} = ?')
             args.append(queryval)
-    wherestr = 'WHERE ' + ' OR '.join(whereparts)
-    return wherestr, args, list(indexers), []
+    wherestr = "WHERE " + " OR ".join(whereparts)
+    return wherestr, args, errors, list(indexers), []
 
 
 def get_indexer(indexers: List,
@@ -380,16 +491,46 @@ def get_indexer(indexers: List,
                 break
         if len(windexedby) == 0:
             # The best index depends on the sorting. This is by default frequency
-            if orderby in indexfields:
+            orderfield = orderby.split(' ')[0]
+            if orderfield in indexfields:
                 windexedby = f"indexed by {indexfields[orderby]}"
         print(f'Force indexer other than autoindex: {windexedby}')
     return windexedby
 
 
-# FIXME: rowlimit to connection class
+def get_querystring(query: Union[str, Dict] = None,
+                    orderby: str = 'w.frequency',
+                    defaultindex: bool = False) -> Tuple[str, List[str], List[str], str, bool]:
+    """Get final query string and other things."""
+    useposx = True
+    if isinstance(query, str):
+        wherestr, args, errors, indexers, notlikeindexers, useposx = parse_querystring(query)
+        # print(errors)
+    elif isinstance(query, dict):
+        defaultindex = True
+        wherestr, args, errors, indexers, notlikeindexers = parse_querydict(query)
+
+    print(wherestr, args)
+    windexedby = "" if defaultindex else get_indexer(indexers, notlikeindexers, orderby)
+    return wherestr, args, errors, windexedby, useposx
+
+
+def get_orderby(orderby: str) -> str:
+    """Get SQLorderby string."""
+    orderdirection = 'DESC'
+    orderparts = orderby.split(' ')
+    if orderparts[0].startswith('w.'):
+        orderby = orderparts[0]
+        if len(orderparts) > 1:
+            if orderparts[1] in ['ASC', 'DESC']:
+                orderdirection = orderparts[1]
+    orderstring = ' '.join([orderby, orderdirection])
+    return orderstring
+
+
+# FIXME: defaultindex to connection class
 # FIXME: lemmas, grams to connection class?
-def get_frequency_dataframe(connection: sqlite3.Connection,
-                            rowlimit: int = 10000,
+def get_frequency_dataframe(dbconnection: DatabaseConnection,
                             orderby: str = 'w.frequency',
                             query: Union[str, Dict] = None,
                             defaultindex: bool = False,
@@ -398,29 +539,32 @@ def get_frequency_dataframe(connection: sqlite3.Connection,
                             grams: bool = False) -> Tuple[pd.DataFrame, int, str]:
     """Get frequencies as dataframe."""
     # FIXME: validate rowlimit
-    # FIXME: make this a class?
 
-    orderdirection = 'DESC'
-    orderparts = orderby.split(' ')
-    if orderparts[0].startswith('w.'):
-        orderby = orderparts[0]
-        if len(orderparts) > 1:
-            if orderparts[1] in ['ASC', 'DESC']:
-                orderdirection = orderparts[1]
+    connection = dbconnection.connection
 
-    table = 'wordfreqs'
-    wherestr = ""
-    args: List[str] = []
+    # FIXME: orderstring takes posx into account
+    orderstring = get_orderby(orderby)
+    # table = 'wordfreqs'
+    # wherestr = ""
+    # args: List[str] = []
 
-    if isinstance(query, str):
-        wherestr, args, indexers, notlikeindexers = parse_querystring(query)
-    elif isinstance(query, dict):
-        defaultindex = True
-        wherestr, args, indexers, notlikeindexers = parse_querydict(query)
+    wherestr, args, errors, windexedby, useposx = get_querystring(query, orderstring, defaultindex)
+    wselects = dbconnection.get_queryselects(useposx)
+    # print(wselects)
 
-    print(wherestr, args)
+    # FIXME: return query errors if so deemed
 
-    # FIXME: get query errors
+    #    if isinstance(query, str):
+    #        wherestr, args, errors, indexers, notlikeindexers = parse_querystring(query)
+    #        print(errors)
+    #    elif isinstance(query, dict):
+    #        defaultindex = True
+    #        wherestr, args, errors, indexers, notlikeindexers = parse_querydict(query)
+
+    #    print(wherestr, args)
+
+    #    windexedby = "" if defaultindex else get_indexer(indexers, notlikeindexers, orderby)
+
     if len(wherestr) == 0:
         return pd.DataFrame(), -1, 'No valid query string'
 
@@ -439,14 +583,24 @@ def get_frequency_dataframe(connection: sqlite3.Connection,
     #            groupby += ", w.nouncase, w.nnumber"
     #            # dropcols.extend(['verbform', 'tense'])
 
+    addfrom = ""
     addselects = ""
     addjoins = ""
 
-    windexedby = "" if defaultindex else get_indexer(indexers, notlikeindexers, orderby)
+    if 'posx' in wselects:
+        groupby = "group by w.lemma, w.form, w.posx, w.feats"
+
+    if 'd.derivation' in wherestr:
+        addfrom += ",derivations d"
+    if 'c.clitic' in wherestr:
+        addfrom += ",clitics c"
 
     if lemmas:
-        addselects += ', l.*'
-        addjoins += ' LEFT JOIN lemmas l ON w.lemma = l.lemma AND w.pos = l.pos'
+        addselects += ', l.lemmafreq, l.lemmalen, l.amblemma'
+        addjoins += ' LEFT JOIN lemmas l ON w.lemma = l.lemma AND w.posx = l.pos'
+
+        addselects += ', 1-lf.formpct as ambform'
+        addjoins += ' LEFT JOIN lemmaforms lf ON w.lemma = lf.lemma AND w.form = lf.form AND w.posx = lf.pos'
 
     if grams:
         aliases = ['i', 'f', 'b']
@@ -459,24 +613,37 @@ def get_frequency_dataframe(connection: sqlite3.Connection,
             addselects += f', {alias}.frequency as {aname}'
             addjoins += f' LEFT JOIN {atable} {alias} ON {alias}.form = {wordcomp}'
 
-    sqlstr = f'SELECT w.*{addselects} FROM {table} w {windexedby} {addjoins} {wherestr} {groupby} ORDER BY {orderby} {orderdirection} LIMIT {rowlimit}'
+    sqlstr = f'SELECT {wselects}{addselects} FROM wordfreqs w{addfrom} {windexedby} {addjoins} {wherestr} {groupby} ORDER BY {orderstring} LIMIT {dbconnection.rowlimit()}'
     print(sqlstr)
     print(args)
-
-    explainer = pd.read_sql_query('explain query plan ' + sqlstr,
-                                  connection,
-                                  params=args)
-    print()
-    print('Query plan:')
-    print(tabulate(explainer, headers=explainer.columns))
 
     querystatus = 0
     querymessage = 'success'
 
-    sql_query = pd.read_sql_query(sqlstr,
-                                  connection,
-                                  params=args)
+    try:
+        explainer = pd.read_sql_query('explain query plan ' + sqlstr,
+                                      connection,
+                                      params=args)
+        print()
+        print('Query plan:')
+        print(tabulate(explainer, headers=explainer.columns))
 
-    df = pd.DataFrame(sql_query)
+        starttime = time.perf_counter()
+        sql_query = pd.read_sql_query(sqlstr,
+                                      connection,
+                                      params=args)
+
+        df = pd.DataFrame(sql_query)
+        endtime = time.perf_counter()
+        print()
+        print(f'{len(df)} rows returned in {endtime - starttime:.1f} seconds')
+
+    except DatabaseError as e:
+        print(str(e))
+        _, errmsg = str(e).split(': ', 1)
+        print(errmsg)
+        logging.exception(e)
+        df = pd.DataFrame()
+        querymessage = errmsg
 
     return df, querystatus, querymessage
