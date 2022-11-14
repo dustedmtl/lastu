36acaa429f05af47a8278873a7c3cc8034a67e1e
Sami Itkonen <si@iki.fi>
2022-11-14 14:48:52 +0200

    schema changes: move/copy len, revform, hood to wordfreqs table

--- sql/forms.sql
+++ sql/forms.sql
@@ -1,21 +1,19 @@
 CREATE TABLE IF NOT EXISTS forms (
        form VARCHAR(256) NOT NULL,
        frequency INTEGER NOT NULL,
-       len INTEGER NOT NULL,
        numforms INTEGER NOT NULL,
        hood INTEGER NOT NULL,
-       revform VARCHAR(256) NOT NULL,
        PRIMARY KEY (form)
 );
 
 CREATE INDEX IF NOT EXISTS idx_forms_form_freq ON forms(form, frequency);
 CREATE INDEX IF NOT EXISTS idx_forms_form_numforms ON forms(form, numforms);
-CREATE INDEX IF NOT EXISTS idx_forms_form_len ON forms(form, len);
-CREATE INDEX IF NOT EXISTS idx_forms_len_form ON forms(len, form);
+--CREATE INDEX IF NOT EXISTS idx_forms_form_len ON forms(form, len);
+--CREATE INDEX IF NOT EXISTS idx_forms_len_form ON forms(len, form);
 CREATE INDEX IF NOT EXISTS idx_forms_form_hood ON forms(form, hood);
-CREATE INDEX IF NOT EXISTS idx_forms_hood_form ON forms(hood, form);
-CREATE INDEX IF NOT EXISTS idx_forms_form_rev ON forms(form, revform);
-CREATE INDEX IF NOT EXISTS idx_forms_rev_form ON forms(revform, form);
+--CREATE INDEX IF NOT EXISTS idx_forms_hood_form ON forms(hood, form);
+--CREATE INDEX IF NOT EXISTS idx_forms_form_rev ON forms(form, revform);
+--CREATE INDEX IF NOT EXISTS idx_forms_rev_form ON forms(revform, form);
 
 CREATE TABLE IF NOT EXISTS lemmaforms (
        lemma VARCHAR(256) NOT NULL,
@@ -27,5 +25,5 @@ CREATE TABLE IF NOT EXISTS lemmaforms (
        PRIMARY KEY (lemma, form, pos)
 );
 
-CREATE INDEX IF NOT EXISTS idx_lemmaforms_lemma_form_freq ON lemmaforms(lemma, form, frequency);
+--CREATE INDEX IF NOT EXISTS idx_lemmaforms_lemma_form_freq ON lemmaforms(lemma, form, frequency);
 CREATE INDEX IF NOT EXISTS idx_lemmaforms_form_frequency ON lemmaforms(form, frequency);
--- sql/wordfreqs2.sql
+++ sql/wordfreqs2.sql
@@ -4,10 +4,14 @@ CREATE TABLE IF NOT EXISTS wordfreqs (
        form VARCHAR(256) NOT NULL,
        pos VARCHAR(16) NOT NULL,
        posx VARCHAR(16) NOT NULL,
+       len INTEGER NOT NULL DEFAULT 0,
        frequency INTEGER NOT NULL,
-       frequencyx INTEGER NOT NULL,
+       frequencyx INTEGER NOT NULL DEFAULT 0,
        feats VARCHAR(256) NOT NULL,
        featid INTEGER NOT NULL,
+       hood INTEGER NOT NULL DEFAULT 0,
+       revform VARCHAR(256) NOT NULL DEFAULT '',
+       ambform FLOAT NOT NULL DEFAULT 0,
        PRIMARY KEY (id)
 );
 
@@ -23,14 +27,21 @@ CREATE INDEX IF NOT EXISTS idx_wordfreqs_featid_form on wordfreqs(featid, form);
 --CREATE INDEX IF NOT EXISTS wfreq ON wordfreqs(frequency);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freq_lemma ON wordfreqs(frequency DESC, lemma);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freq_form ON wordfreqs(frequency DESC, form);
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_freq_revform ON wordfreqs(frequency DESC, revform);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freq_pos ON wordfreqs(frequency DESC, pos);
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_freq_len ON wordfreqs(frequency DESC, len);
 
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freqx_lemma ON wordfreqs(frequencyx DESC, lemma);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freqx_form ON wordfreqs(frequencyx DESC, form);
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_freqx_revform ON wordfreqs(frequencyx DESC, revform);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_freqx_posx ON wordfreqs(frequencyx DESC, posx);
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_freqx_len ON wordfreqs(frequency DESC, len);
 
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_form_pos ON wordfreqs(form, pos);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_form_posx ON wordfreqs(form, posx);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_form_freq ON wordfreqs(form, frequency);
 CREATE INDEX IF NOT EXISTS idx_wordfreqs_form_freqx ON wordfreqs(form, frequencyx);
+
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_form_rev ON wordfreqs(form, revform);
+CREATE INDEX IF NOT EXISTS idx_wordfreqs_rev_form ON wordfreqs(revform, form);
 
