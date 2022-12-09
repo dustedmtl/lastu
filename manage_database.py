f2793c61cf3e32b44cf85e3761dedc92a1216f31
Sami Itkonen <si@iki.fi>
2022-12-09 21:48:28 +0200

    database management: pruning and concat

--- manage_database.py
+++ manage_database.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Generate init/fin/bigram frequencies to database."""
+"""Manage database."""
 
 # pylint: disable=invalid-name, consider-using-with
 
@@ -8,75 +8,182 @@
 from os.path import exists
 import sys
 import argparse
-# import shutil
-# from sqlite3 import IntegrityError
-from tqdm import tqdm
-
-from lib import dbutil
+from sqlite3 import IntegrityError
+from shutil import copy
+import math
+from tqdm.autonotebook import tqdm
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('prune-database')
+from lib import dbutil, buildutil
 
-parser = argparse.ArgumentParser(prog='prune-database',
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('manage-database')
+
+parser = argparse.ArgumentParser(prog='manage-database',
                                  description='Prune database')
 
 parser.add_argument('-i', '--input',
                     type=str,
+                    nargs='+',
                     required=True,
-                    help='input DB')
+                    help='input database(s)')
 
 parser.add_argument('-o', '--output',
                     type=str,
-                    required=True,
-                    help='input DB')
+                    # required=True,
+                    help='output database(s)')
 
-parser.add_argument('-n', '--newfile',
-                    action='store_true',
-                    help='New db file')
+parser.add_argument('-c', '--cmd',
+                    type=str,
+                    required=True,
+                    help='Command to execute')
 
 parser.add_argument('-f', '--frequency',
                     type=int,
-                    default=10,
-                    help='Frequency filter')
+                    default=1,
+                    help='Frequency filter for pruning')
 
 parser.add_argument('-v', '--verbose',
                     action='store_true',
                     help='Verbose')
 
+parser.add_argument('-e', '--allowempty',
+                    action='store_true',
+                    help='Allow empty target file')
+
+parser.add_argument('-y', '--yes',
+                    action='store_true',
+                    help='Do not ask for confirmation')
+
 args = parser.parse_args()
+cmd = args.cmd
 
-if not exists(args.input):
-    logger.warning('No such file: %s', args.input)
-    sys.exit()
-
-sqlcon = None
-if not exists(args.output):
-    if args.newfile:
-        print(f'Creating database at {args.output}')
-        sqlcon = dbutil.get_connection(args.output)
-        cursor = sqlcon.cursor()
-        with open('sql/wordfreqs.sql', 'r', encoding='utf8') as schemafile:
-            sqldata = schemafile.read()
-            cursor.executescript(sqldata)
-    else:
-        logger.warning('No such file: %s', args.output)
+if cmd == 'prune':
+    # only take the first input into account
+    inputfile = args.input[0]
+    if not exists(inputfile):
+        logger.warning('No such file: %s', inputfile)
         sys.exit()
 
-if not sqlcon:
-    sqlcon = dbutil.get_connection(args.output)
-origcon = dbutil.get_connection(args.input)
+    print(f'Using {inputfile} as source database')
+    sqlcon = dbutil.get_connection(inputfile)
+    # Determine how much stuff will be deleted
+    total = dbutil.adhoc_query(sqlcon, "select count(*) from wordfreqs")
+    topurge = dbutil.adhoc_query(sqlcon, f"select count(*) from wordfreqs where frequency <= {args.frequency}")
+    print(f'About to delete {topurge[0][0]} rows of {total[0][0]}')
+    if not args.yes:
+        ok = input('Ok? y/N: ')
+        if not ok.lower().startswith('y'):
+            print('Not doing anything')
+            sys.exit()
 
-selectsql = "select * from wordfreqs where frequency >= ?"
-insertpat = "insert into wordfreqs values (%s)"
+    # Copy database if so desired
+    if args.output:
+        if exists(args.output):
+            raise FileExistsError(args.output)
+        print(f'Copying {inputfile} to {args.output}')
+        copy(inputfile, args.output)
+        sqlcon = dbutil.get_connection(args.output)
 
-src = origcon.cursor()
-dest = sqlcon.cursor()
+    # Drop extraneous information:
+    #  - helper tables
+    #  - wordfreqs indexes, except unique
+    #  - nullify computed wordfreqs info: hood, ambform
 
-rows = src.execute(selectsql, [args.frequency])
-for row in tqdm(rows.fetchall()):
-    num = len(row)
-    ins = insertpat % ','.join(['?'] * num)
-    dest.execute(ins, row)
-    sqlcon.commit()
-    # break
+    print('Dropping helper tables...')
+    buildutil.drop_helper_tables(sqlcon)
+    print('Dropping wordfreqs indexes...')
+    buildutil.drop_indexes(sqlcon, 'wordfreqs')
+    print('Nullifying computed information...')
+    buildutil.nullify_wordfreqs(sqlcon)
+
+    # Deletion
+    print('Dropping matching rows...')
+    buildutil.delete_rows(sqlcon, args.frequency)
+
+    # Rebuild
+    print('Re-adding indexes and tables...')
+    buildutil.add_schema(sqlcon, 'wordfreqs_indexes.sql')
+    buildutil.add_schema(sqlcon, 'features.sql')
+    print('Re-linking features information...')
+    buildutil.add_features(sqlcon)
+
+    print('Remember to add grams and helper tables!')
+
+if cmd == 'concat':
+    if not args.output:
+        print('Need to provide output file')
+        sys.exit()
+
+    notfound = []
+    for fn in args.input:
+        if not exists(fn):
+            notfound.append(fn)
+
+    if notfound:
+        logger.warning('Missing input files: %s', notfound)
+        sys.exit()
+
+    # New database
+    if args.output:
+        if exists(args.output):
+            if args.allowempty:
+                print(f'Target file {args.output} already exists, allowing')
+            else:
+                raise FileExistsError(args.output)
+        else:
+            print(f'Creating new database at {args.output}')
+            buildutil.create_database(args.output)
+
+    targetcon = dbutil.get_connection(args.output)
+    cursor = targetcon.cursor()
+
+    print(f'Inserting data from {len(args.input)} files')
+    insertpat = "insert into wordfreqs (%s) values (%s) on conflict(lemma, form, pos, feats) do update set frequency = frequency + excluded.frequency"
+    insertsql = ""
+    columns = None
+    chunklen = 100000
+    # chunklen = 1
+
+    for fn in args.input:
+        print(f'Inserting data from {fn}...')
+        sqlcon = dbutil.get_connection(fn)
+        dbdata = dbutil.adhoc_query(sqlcon, "SELECT * FROM wordfreqs", todf=True).drop('id', axis=1)
+        if columns is None:
+            columns = dbdata.columns
+            print(columns)
+            qs = ['?'] * len(columns)
+            # print(qs)
+            insertsql = insertpat % (', '.join(columns), ','.join(qs))
+            print(insertsql)
+
+        totwordchunks = math.ceil(len(dbdata)/chunklen)
+
+        print(f'Inserting {len(dbdata)} rows in {totwordchunks} chunks...')
+
+        for chunk in tqdm(dbutil.chunks(dbdata, chunklen=chunklen),
+                          total=totwordchunks):
+            try:
+                cursor.executemany(insertsql, chunk)
+                targetcon.commit()
+            except IntegrityError as e:
+                # this is not ok
+                logging.exception(e)
+                targetcon.rollback()
+            # break
+        # break
+
+
+# selectsql = "select * from wordfreqs where frequency >= ?"
+
+# src = origcon.cursor()
+# dest = sqlcon.cursor()
+
+# rows = src.execute(selectsql, [args.frequency])
+# for row in tqdm(rows.fetchall()):
+#    num = len(row)
+#    ins = insertpat % ','.join(['?'] * num)
+#    dest.execute(ins, row)
+#    sqlcon.commit()
+#    # break
